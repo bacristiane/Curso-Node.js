a5b5ef393bf27d32a685bbd22ff4387e25fae66f
bacristiane <barbara.cristianefs@gmail.com>
2022-04-18 10:43:23 -0300

    Função para resgatar todas as adoções

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -97,4 +97,15 @@ module.exports = class PetController {
     res.status(200).json({pets: pets})
 
     }
+
+    static async getAllUserAdoptions(req,res){
+
+        const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    const pets = await Pet.find({'adopter._id': user._id}).sort('-createdAt')
+
+    res.status(200).json({pets: pets})
+
+    }
 }
