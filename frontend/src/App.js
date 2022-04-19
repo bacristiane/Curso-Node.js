af310c3a7aa129e005c2529e4a76d3a8a0f7f42b
bacristiane <barbara.cristianefs@gmail.com>
2022-04-19 09:27:40 -0300

    Estruturando navbar e footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,22 @@ import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 import Home from './components/pages/Home';
 
+//components
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
           
         <Route path="/register" element={<Register />} />
           
         <Route path="/" element={<Home />} />
-        
       </Routes>
+      <Footer />
     </Router>
   )
 }
---
+++ frontend/src/components/layouts/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return(
+        
+        <footer>
+            <p>
+                <span.bold>
+                    
+                </span.bold>
+            </p>
+        </footer>
+    )
+    }
+    
+    export default Footer
