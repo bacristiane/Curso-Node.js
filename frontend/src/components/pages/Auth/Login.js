6b6179a23cd0eb833e5572fbe8a184f3f93c2205
bacristiane <barbara.cristianefs@gmail.com>
2022-04-21 18:34:54 -0300

    Criando formulário de login

--- frontend/src/components/pages/Auth/Login.js
+++ frontend/src/components/pages/Auth/Login.js
@@ -1,9 +1,51 @@
+import Input from '../../form/Input'
+import styles from '../../form/Form.module.css'
+import {Link} from 'react-router-dom'
+
+import { useState } from 'react'
+import { useContext } from 'react'
+
+//context
+import { Context } from '../../../context/UserContext'
+
+
 function Login() {
+
+    function handleChange(e){
+        /* setUser({...user, [e.target.name]: e.target.value}) */
+
+    }
+    function handleSubmit(e){
+        /* e.preventDefault()
+        //enviar usuário para o banco
+        register(user) */
+    }
     return(
-        <section>
-            <h1>
-                Login
-            </h1>
+        <section className={styles.form_container}>
+            <h1>Entrar</h1>
+            <form onSubmit={handleSubmit}>
+                <Input 
+                text="Email"
+                type="email"
+                name="email"
+                placeholder="Digite o seu email"
+                handleOnChange={handleChange}
+                />
+                <Input 
+                text="Senha"
+                type="password"
+                name="password"
+                placeholder="Digite o sua senha"
+                handleOnChange={handleChange}
+                />
+                <input
+                type="submit"
+                value="Acessar"
+                />
+            </form>
+            <p>
+                Não tem conta? <Link to="/register"> Clique aqui.</Link>
+            </p>
         </section>
     )
     }
