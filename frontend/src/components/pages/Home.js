7fed2336492452cac8e350c8694bfc8af00c28d4
bacristiane <barbara.cristianefs@gmail.com>
2022-04-27 12:16:34 -0300

    Exibindo pets na home

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import api from '../../utils/api'
-import {Link} from 'react-router-dom'
-import {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import styles from './Home.module.css'
 
 function Home() {
@@ -11,16 +11,36 @@ function Home() {
         api.get('/pets').then((response) => {
             setPets(response.data.pets)
         })
-    },[])
+    }, [])
 
 
-return(
-    <section>
-        <h1>
-            Home
-        </h1>
-    </section>
-)
+    return (
+        <section>
+            <div>
+                <h1>Adote um pet</h1>
+                <p>Veja os detalhes de cada um e conheça o tutor dele.</p>
+            </div>
+            <div>
+                {pets.length > 0 && 
+                    pets.map((pet) => (
+                        <div>
+                            <p> Imagem do PET</p>
+                            <h3>{pet.name}</h3>
+                            <p><span className='bold'>Peso:</span>{pet.weight}kg</p>
+                           {pet.available ? (
+                               <Link to={`pet/${pet._id}`}>Mais detalhes</Link>
+                           ) : (
+                               <p>Adotado</p>
+                           ) }
+                        </div>
+
+                ))}
+                {pets.length === 0 && (
+                    <p> Não há pets cadastrados ou disponíveis para adoção</p>
+                )}
+            </div>
+        </section>
+    )
 }
 
 export default Home
