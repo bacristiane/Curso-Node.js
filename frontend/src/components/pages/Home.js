84cc684f33e97d9567130c5a51d4d83688f8813b
bacristiane <barbara.cristianefs@gmail.com>
2022-04-27 12:37:39 -0300

    CSS home

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -16,21 +16,25 @@ function Home() {
 
     return (
         <section>
-            <div>
+            <div className={styles.pet_home_header}>
                 <h1>Adote um pet</h1>
                 <p>Veja os detalhes de cada um e conheça o tutor dele.</p>
             </div>
-            <div>
+            <div className={styles.pet_container}>
                 {pets.length > 0 && 
                     pets.map((pet) => (
-                        <div>
-                            <p> Imagem do PET</p>
+                        <div className={styles.pet_card}>
+                            <div 
+                            style={{
+                                backgroundImage:`url(${process.env.REACT_APP_API}/images/pets/${pet.images[0]})`
+                            }}
+                            className={styles.pet_card_image}></div>
                             <h3>{pet.name}</h3>
                             <p><span className='bold'>Peso:</span>{pet.weight}kg</p>
                            {pet.available ? (
                                <Link to={`pet/${pet._id}`}>Mais detalhes</Link>
                            ) : (
-                               <p>Adotado</p>
+                               <p className={styles.adopted_text}>Adotado</p>
                            ) }
                         </div>
 
