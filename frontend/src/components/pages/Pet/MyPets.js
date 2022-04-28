dceafe2197678990eba80a39bcecb38b9ea7731a
bacristiane <barbara.cristianefs@gmail.com>
2022-04-28 10:24:45 -0300

    Conclusão de adoção

--- frontend/src/components/pages/Pet/MyPets.js
+++ frontend/src/components/pages/Pet/MyPets.js
@@ -46,6 +46,25 @@ function MyPets() {
         setFlashMessage(data.message,msgType)
     }
 
+    async function concludeAdoption(id) {
+
+        let msgType='success'
+        const data = await api.patch(`/pets/conclude/${id}`, {
+            headers:{
+                token: `${JSON.parse(token)}`,
+            }
+
+        }).then((response) => {
+            return (response.data)
+        }).catch((err) =>{
+            msgType='error'
+            return err.response.data
+
+        })
+
+        setFlashMessage(data.message,msgType)
+    }
+
     return(
         <section >
             <div className={styles.petlist_header}>
@@ -66,7 +85,9 @@ function MyPets() {
                         {pet.available ? 
                         (<>
                         {pet.adopter && (
-                            <button className={styles.conclude_btn}> Concluir adoção </button>
+                            <button className={styles.conclude_btn} onClick={() => {
+                                concludeAdoption(pet._id)
+                            }}> Concluir adoção </button>
                         )}
                         <Link to={`/pet/edit/${pet._id}`}> Editar</Link>
                         <button onClick={() => {
